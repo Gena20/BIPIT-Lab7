98f55e5cd91ec0e5844169c0a891a9110c3da363
gena <peryatin.gena@yandex.ru>
2020-11-16 13:25:02 +0500

    all done

---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.lbMsgs = new System.Windows.Forms.ListBox();
+            this.tbMsg = new System.Windows.Forms.TextBox();
+            this.lableUserName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(238, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(147, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(75, 15);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(131, 20);
+            this.tbUserName.TabIndex = 1;
+            // 
+            // lbMsgs
+            // 
+            this.lbMsgs.FormattingEnabled = true;
+            this.lbMsgs.Location = new System.Drawing.Point(12, 41);
+            this.lbMsgs.Name = "lbMsgs";
+            this.lbMsgs.Size = new System.Drawing.Size(373, 147);
+            this.lbMsgs.TabIndex = 2;
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Location = new System.Drawing.Point(12, 194);
+            this.tbMsg.Multiline = true;
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.Size = new System.Drawing.Size(373, 51);
+            this.tbMsg.TabIndex = 1;
+            this.tbMsg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbMsg_KeyDown);
+            // 
+            // lableUserName
+            // 
+            this.lableUserName.AutoSize = true;
+            this.lableUserName.Location = new System.Drawing.Point(12, 18);
+            this.lableUserName.Name = "lableUserName";
+            this.lableUserName.Size = new System.Drawing.Size(57, 13);
+            this.lableUserName.TabIndex = 3;
+            this.lableUserName.Text = "UserName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 257);
+            this.Controls.Add(this.lableUserName);
+            this.Controls.Add(this.lbMsgs);
+            this.Controls.Add(this.tbMsg);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.ListBox lbMsgs;
+        private System.Windows.Forms.TextBox tbMsg;
+        private System.Windows.Forms.Label lableUserName;
+    }
+}
+
---
+++ ChatClient/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChatClient.ServiceChat;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form, IServiceChatCallback
+    {
+        bool isConnected;
+        ServiceChatClient client;
+        int userId;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                userId = client.Connect(tbUserName.Text);
+                lbMsgs.Items.Clear();
+                lbMsgs.Items.AddRange(client.LoadHistory());
+            }
+        }
+
+        void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(userId);
+                client = null;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (isConnected) DisconnectUser();
+            else ConnectUser();
+
+            tbUserName.Enabled = isConnected;
+            isConnected = !isConnected;
+            btnConnect.Text = isConnected ? "Disconnect" : "Connect";
+        }
+
+        public void MsgCallback(string msgText)
+        {
+            lbMsgs.Items.Add(msgText);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void tbMsg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && client != null)
+            {
+                client.SendMsg(tbMsg.Text, userId);
+                tbMsg.Text = string.Empty;
+            }
+        }
+
+    }
+}
